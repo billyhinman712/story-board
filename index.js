ae9a2f503001adc636ecd74ccfa9a483cd3db686
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-08-23 11:25:35 -0700

    index.js boilerplate

---
+++ index.js
@@ -0,0 +1,22 @@
+//require needed modules
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+
+//declare app variable
+var app = express();
+
+//set and use statments
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(ejsLayouts);
+
+//define routes
+app.get('/', function(req, res){
+	res.send('hi from home route');
+});
+
+//listen to port 3000
+app.listen(3000);
